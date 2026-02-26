48456d54a07f34ca8ff6829253442247252d98ae
Eduardo Raymond Beniste <eduardoraymondbeniste999@gmail.comm>
2026-02-26 10:24:16 -0300

    Configuração final de conexão SSL

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'todolist_db',
+    port: process.env.DB_PORT || 4000,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+module.exports = pool.promise();
